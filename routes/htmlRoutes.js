ab40d83ac7642ed2b7d0269fb2e350a9cf2ceb9f
elcid00 <elcid00@aol.com>
2019-04-14 22:37:55 -0400

    a few changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,43 +1,45 @@
-var db = require("../models");
-var path = require("path");
+var db = require('../models');
+var path = require('path');
 
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+var isAuthenticated = require('../config/middleware/isAuthenticated');
 
-module.exports = function(app) {  
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+module.exports = function(app) {
+	//AUTHENTICATION SETUP
 
+	app.get('/', function(req, res) {
+		// If the user already has an account send them to the members page
+		if (req.user) {
+			res.redirect('/members');
+		}
+		res.sendFile(path.join(__dirname, '../public/login.html'));
+	});
 
+	app.get('/signup', function(req, res) {
+		// If the user already has an account send them to the members page
+		if (req.user) {
+			res.redirect('/members');
+		}
+		res.sendFile(path.join(__dirname, '../public/signup.html'));
+	});
 
-  //AUTHENTICATION SETUP 
+	// Here we've add our isAuthenticated middleware to this route.
+	// If a user who is not logged in tries to access this route they will be redirected to the signup page
+	app.get('/members', isAuthenticated, function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/members.html'));
+	});
 
-  app.get("/", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
+	// 	// Load index page
+	// app.get('/', function(req, res) {
+	// 	db.Example.findAll({}).then(function(dbExamples) {
+	// 		res.render('index', {
+	// 			msg: 'Welcome!',
+	// 			examples: dbExamples
+	// 		});
+	// 	});
+	// });
 
-  app.get("/signup", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
-
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
-
-
+	// // Render 404 page for any unmatched routes
+	// app.get('*', function(req, res) {
+	// 	res.render('404');
+	// });
 };
