3edbfb1388f2f57179513c577f5b5a055e10aeee
Rhino <ryanharrisfl@gmail.com>
2019-04-16 18:07:26 -0400

    cleaned up code in models and api-routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -24,12 +24,12 @@ module.exports = function(app) {
 
 	// Here we've add our isAuthenticated middleware to this route.
 	// If a user who is not logged in tries to access this route they will be redirected to the signup page
-	// app.get('/members', isAuthenticated, function(req, res) {
-	// 	res.sendFile(path.join(__dirname, '../public/members.html'));
-	// });
+	app.get('/members', isAuthenticated, function(req, res) {
+	res.sendFile(path.join(__dirname, '../public/members.html'));
+	 });
 
-	// 	// Load index page
-	// app.get('/', function(req, res) {
-	// 	res.render("index");
-	// });
+	//Load index page
+	app.get('/', function(req, res) {
+	res.render("index");
+	 });
 };
